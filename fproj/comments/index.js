075ce6d0fa57cd440d11d0e8cb7fed55e6bd6ff6
dcca <dcca12@gmail.com>
2020-06-01 17:56:07 -0300

    add react basic set up, add cors for server side server

--- fproj/comments/index.js
+++ fproj/comments/index.js
@@ -1,30 +1,32 @@
-const express = require('express');
-const { randomBytes } = require('crypto');
-const bodyParser = require('body-parser');
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors());
 
-const commentsByPostId = {}
+const commentsByPostId = {};
 
-app.get('/posts/:id/comments', (req, res, next) => {
-    res.send(commentsByPostId[req.params.id] || []);
-})
+app.get("/posts/:id/comments", (req, res, next) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
 
-app.post('/posts/:id/comments', (req, res, next) => {
-    const commentId = randomBytes(4).toString('hex');
+app.post("/posts/:id/comments", (req, res, next) => {
+  const commentId = randomBytes(4).toString("hex");
 
-    const { content } = req.body
+  const { content } = req.body;
 
-    const comments = commentsByPostId[req.params.id] || [];
+  const comments = commentsByPostId[req.params.id] || [];
 
-    comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content });
 
-    commentsByPostId[req.params.id] = comments;
+  commentsByPostId[req.params.id] = comments;
 
-    res.status(201).send(comments);
-})
+  res.status(201).send(comments);
+});
 
 app.listen(4001, () => {
-    console.log('Listenning on 4001')
-})
+  console.log("Listenning on 4001");
+});
